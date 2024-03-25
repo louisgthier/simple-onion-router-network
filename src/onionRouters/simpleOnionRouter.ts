32b998b021b41f43eaac6be857afa378da0b949f
Louis Gauthier <louis@dgwave.net>
2024-03-25 19:11:41 +0100

    Finish implementation

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,6 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { generateRsaKeyPair, exportPubKey, exportPrvKey, exportSymKey } from "../crypto";
+import { generateRsaKeyPair, exportPubKey, exportPrvKey, exportSymKey, importSymKey, rsaDecrypt, symDecrypt } from "../crypto";
 import { BASE_ONION_ROUTER_PORT, REGISTRY_PORT } from "../config";
 import { log } from "console";
 
@@ -25,6 +25,10 @@ export async function simpleOnionRouter(nodeId: number) {
     },
   });
 
+  var lastReceivedEncryptedMessage: string | null = null;
+  var lastReceivedDecryptedMessage: string | null = null;
+  var lastMessageDestination: number | null = null;
+
   // GET /status
   onionRouter.get("/status", (req, res) => {
     res.send("live");
@@ -32,17 +36,17 @@ export async function simpleOnionRouter(nodeId: number) {
 
   // GET /getLastReceivedEncryptedMessage
   onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
-    res.json({ result: null });
+    res.json({ result: lastReceivedEncryptedMessage });
   });
 
   // GET /getLastReceivedDecryptedMessage
   onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
-    res.json({ result: null });
+    res.json({ result: lastReceivedDecryptedMessage });
   });
 
   // GET /getLastMessageDestination
   onionRouter.get("/getLastMessageDestination", (req, res) => {
-    res.json({ result: null });
+    res.json({ result: lastMessageDestination });
   });
 
   // GET /getPrivateKey
@@ -53,7 +57,41 @@ export async function simpleOnionRouter(nodeId: number) {
     });
   });
 
+  // POST /message
+  onionRouter.post("/message", async (req, res) => {
+    const { message } = req.body;
 
+    lastReceivedEncryptedMessage = message;
+
+    // Encrypted key is the first 344 characters
+    const encryptedSymmetricKey = message.slice(0, 344);
+    const encryptedDestinationAndMessage = message.slice(344);
+
+    // Decrypt the symmetric key
+    const symmetricKey = await rsaDecrypt(encryptedSymmetricKey, keyPair.privateKey);
+
+    // Decrypt the destination and message
+    const destinationAndMessage = await symDecrypt(symmetricKey, encryptedDestinationAndMessage);
+
+    // Split the destination and message
+    const destination = parseInt(destinationAndMessage.slice(0, 10));
+    const decryptedMessage = destinationAndMessage.slice(10);
+
+    lastMessageDestination = destination;
+    lastReceivedDecryptedMessage = decryptedMessage;
+
+
+    // Make POST request to localhost:${destination}/message
+    await fetch(`http://localhost:${destination}/message`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: decryptedMessage }),
+    });
+
+    res.send("success");
+  });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
